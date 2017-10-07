881134aabd78eafdd1b8ddb3d4b72bbee2bbcee7
kenta-s <knt01222@gmail.com>
2017-10-07 11:09:14 +0900

    Add import

--- src/Model/User.hs
+++ src/Model/User.hs
@@ -1,3 +1,30 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TemplateHaskell #-}
 
-module Model.User where
+module Model.User
+    ( NewUser(NewUser, nuName, nuPassword)
+    , nuName'
+    , nuPassword'
+    , insertUser
+    , selectUser
+    )
+    where
+
+import Control.Exception (catch)
+import Crypto.BCrypt
+    ( hashPasswordUsingPolicy
+    , slowerBcryptHashingPolicy
+    , validatePassword
+    )
+import qualified Data.ByteString as BS
+import Data.Text (pack, unpack)
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import Database.HDBC
+    ( IConnection
+    , SqlError
+    , withTransaction
+    )
+import Database.HDBC.Query.TH (makeRecordPersistableDefault)
+import qualified Database.HDBC.Record as DHR
+import qualified Database.HDBC.Relational.Query as HRR
+import qualified Entity.User as User
+import System.IO (hPrint, hPutStrLn, stderr)
